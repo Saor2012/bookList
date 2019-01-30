85dc24e1098c9f38146e66964c0025fe193d1da2
Igor Oliinyk <igor.oleynikdp@gmail.com>
2019-01-30 14:24:35 +0200

    Добавил валидацию и уведомления при добавлении книги

--- app.js
+++ app.js
@@ -27,6 +27,33 @@ UI.prototype.addBookToList = function(book) {
   list.appendChild(row)
 }
 
+// Show Alert
+UI.prototype.showAlert = function(message, className) {
+  // Create div
+  const div = document.createElement('div')
+  // Add classes
+  div.className = `alert ${className}`
+  // Add text
+  div.appendChild(document.createTextNode(message))
+  // Get parent
+  const container = document.querySelector('.container')
+  // Get form
+  const form = document.querySelector('#book-form')
+  // Insert form
+  container.insertBefore(div, form)
+
+  setTimeout(function() {
+    document.querySelector('.alert').remove()
+  }, 3000)
+}
+
+// Clear Fields
+UI.prototype.clearFields = function() {
+  document.getElementById('title').value = ''
+  document.getElementById('author').value = ''
+  document.getElementById('isbn').value = ''
+}
+
 // Event listeners
 
 document.getElementById('book-form').addEventListener('submit', function(e) {
@@ -42,8 +69,21 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
   // Instanciate UI
   const ui = new UI()
 
-  // Add book to list
-  ui.addBookToList(book)
+  // Validate
+
+  if (title === '' || author === '' || isbn === '') {
+    // Error Alert
+    ui.showAlert('Please fill in all fields', 'error')
+  } else {
+    // Add book to list
+    ui.addBookToList(book)
+
+    // Show Success
+    ui.showAlert('Book added', 'success')
+    // Clear fields
+    ui.clearFields()
+  }
+
 
   e.preventDefault()
 })
